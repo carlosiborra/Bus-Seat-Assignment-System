acc0ca692ef0425bd6714454072b752ad33be081
MiguelMHR <100451258@alumnos.uc3m.es>
2022-12-07 12:20:19 +0100

    Novedades: - Creada la estructura de A*:     * Se han asumido las funciones de coste, parsear_resultado, restricciones de movilidad reducida, etc     - Se han actualizado las funciones de restantes, is_goal, expandir, esta_en_lista y la función principal de a_estrella

--- parte-2/ASTARColaBus.py
+++ parte-2/ASTARColaBus.py
@@ -1,137 +1,142 @@
-# Restricciones:
-#  - Los alumnos con movilidad reducida tardan tres veces mas en montar en el autobús que el resto de
-# alumnos.
-#  - El alumno que se encuentre en la cola justo detras de un alumno con movilidad reducida debera ayudarle
-# a subir al autobus, por lo que no puede haber dos alumnos con movilidad reducida uno a continuacion
-# del otro ni ocupando la ultima posición en la cola.
-#  - Cuando un alumno ayuda a subir a otro con movilidad reducida el tiempo que tardan los dos es el tiempo
-# que tarda el alumno con movilidad reducida puesto que suben a la vez.
-#  - Un alumno conflictivo duplicará el tiempo necesario para subir al autobús tanto del compañero que se
-# encuentre justo delante de él como del compañero que se encuentre justo detrás, de este modo, si un
-# alumno conflictivo ayuda a un alumno con movilidad reducida a subir al autobus, el tiempo empleado por
-# los dos se duplicara respecto al tiempo que se invertiría si quien ayudase fuese un alumno no conflictivo.
-#  - Un alumno conflictivo duplicara el tiempo invertido para subir al autobús de todos aquellos alumnos que,
-# encontrándose en la cola después de él, tengan asignados asientos en el autobús con una numeración
-# superior a la suya.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" importing libraries """
 
 
-# AGENTE VIAJERO: Se debe visitar solo 1 vez cada nodo y todos los nodos deben ser visitados, volviendo al nodo inicial
-# Ruta con coste menor acumulado
-# link interesante: https://knuth.uca.es/moodle/mod/page/view.php?id=3417
+# Importamos las listas para poder obtener los datos del archivo
+# El diccionario de un archivo .prob de un path
+# Otra función para parsear el resultado de la función anterior a una lista
 
-# Factor de ramificación > 10
-
-dict_posiciones = {'4XR': 2, '3XR': 16, '2XR': 18, '1XR': 20}
-
-# Variables
-nodos_gen = 0
-nodos_expand = 0
-# Cola vacía, y los alumnos obtenidos de la parte 1 se usarán para irlos metiendo en la cola
+# TODO: Añadir las funciones externas necesarias para que funcione A* -> parsear_resultado, coste, mov_red_seguidos, mov_red_final, etc
+# TODO: Añadir cola_total sacado del resultado de la parte 1
 
 
-class Nodo():
-    """ Clase nodos para el algoritmo A* """
+def heuristica1(cola_restante) -> callable:
+    """ Función heurística que devuelve el número de alumnos que quedan por subir al autobús """
+    return len(cola_restante)
 
-    def __init__(self, padre=None, id=None, g=0, h=0):
+
+class Estado():
+    """ Clase de los nodos para el algoritmo A* """
+
+    def __init__(self, padre=None, cola_bus: list = [], cola_total: list = []):
         """Constructor de la clase nodo"""
-        self.id = id
         self.padre = padre
-        self.heur = heuristica
-        self.coste = coste
-        self.coste_f = self.coste + self.heur  # f(n) = g(n) + h(n)
-        # TODO: añadir variables del nodo que hace que sea inequívoco
+        self.cola_bus = cola_bus
+        self.cola_restante = restantes(self.cola_bus, cola_total)
+        self.coste_h = heuristica1(self.cola_restante)
+        self.coste_g = coste(self.cola_bus)
+        self.coste_f = self.coste_g + self.coste_h  # f(n) = g(n) + h(n)
 
-    def __eq__(self, otro):
-        """Compara dos nodos -> para poder saber si se ha expandido o no. No se compara ni el coste ni el valor h ni el nodo padre"""
-        return ((self.id == otro.id))
-
-
-def heuristica(estado):
-    """Función heurística que devuelve el número de alumnos que no están en su asiento"""
-    # Función heurística que devuelve el número de alumnos que faltan por subir al autobús
-    return len(open_list)
+    def __eq__(self, other):
+        """ Función que compara dos nodos """
+        return self.cola_restante == other.cola_restante
 
 
-def coste(nodo, alumno):
-    """Función que calcula el coste de un nodo"""
-    # Si el alumno es de movilidad reducido, tarda 3 veces más en subir al autobús
-    estado = nodo.id
+def restantes(cola_bus, cola_total) -> list:
+    """ Función que devuelve una lista con los alumnos que faltan por subir al autobús """
+    if cola_bus is None:
+        return cola_total
+    return [alumno for alumno in cola_total if alumno not in cola_bus]
 
 
-def expandir(nodo):
-    """Función que devuelve una lista de nodos hijos de un nodo dado"""
-
-
-def is_goal(estado):
-    """Función que devuelve True si el estado es meta y False en caso contrario (heurística = 0)"""
-    if heuristica(estado) == 0:
+def is_goal(estado) -> bool:
+    """Función que determina si un estado es meta en base a la heurística"""
+    if (estado.coste_h == 0):
         return True
     else:
         return False
 
 
-def esta_en_cola(cola, nodo):
-    """Función que devuelve True si un nodo está en una cola y False en caso contrario"""
+def expandir(estado, cola_total) -> list:
+    """ Función que consigue los estados posibles de expansión en base a un estado """
+    if len(estado.cola_bus) > 1 and (mov_reducida_seguidos(estado.cola_bus) or mov_reducida_final(estado.cola_bus, len(estado.cola_restante))):
+        print('ERROR: Movilidad reducida seguida o al final')
+        return []  # No se han añadido hijos ya que no se puede seguir por esta rama
+    else:
+        hijos = []
+        for elem in estado.cola_restante:
+            cola_nueva = estado.cola_bus + [elem]
+            nuevo_estado = Estado(estado, cola_nueva,
+                                  restantes(cola_nueva, cola_total))
+            hijos.append(nuevo_estado)
+        return hijos
 
 
-def a_estrella(estado_inicial: list):
-    """
-    Función que implementa el algoritmo A*
-    idea: Ordenar por coste
-    (sort()), ya que en a estrella las, listas han de estar ordenadas de menor a mayor 
-    e ir haciendo pop(0) para ir sacando el de menor coste
-    cuidado!: Se pueden añadir nodos iguales pero con costes diferentes, comprobar siempre!
-    """
-    ###  Creación de las variables globales  ###
-    open_list = estado_inicial      # Lista de nodos posibles de expansión
-    closed_list = []                # Lista de nodos expandidos
-    # decision_list = []            # Lista de nodos que van a competir en cada iteración por acabar en la closed list
+def esta_en_lista(estado, lista) -> bool:
+    """Función que determina si un estado está en la lista determinada"""
+    for state in lista:
+        if estado == state:
+            return True
+        else:
+            return False
+
+
+def a_estrella(estado_inicial, cola_total):
+    """ Función que implementa el algoritmo A* """
+    ####  variables iniciales  ####
+    nodes_gen = 1                   # Número de nodos generados
+    # Metemos en la lista abierta el estado inicial
+    open_list = [estado_inicial]  # Lista abierta con los nodos a expandir
+    closed_list = []                # Lista de nodos espandidos
     goal = False                    # Variable que indica si se ha llegado al estado meta
 
-    # Creamos el nodo inicial
-    nodo_inicial = Nodo(None, closed_list, 0, heuristica(open_list))
-
+    ####  bucle de ejecución de A*  ####
     while (not goal or len(open_list) >= 0):
-        nodo_actual = open_list.pop(0)
-        for elem in open_list:
-            if elem.coste < nodo_actual.coste_f:
-                nodo_actual = elem
-
-        if is_goal(nodo_actual):
+        estado_actual = open_list.pop()
+        if is_goal(estado_actual):
             goal = True
             break
         else:
-            closed_list.append(nodo_actual)
-            # Se compara por todos los hijos
-            for elem in nodo_actual.expandir():
-                if not esta_en_cola(closed_list, elem):
-                    if not esta_en_cola(open_list, elem):
-                        open_list.append(elem)
+            hijos = expandir(estado_actual, cola_total)
+            if not hijos:
+                continue
+            else:
+                closed_list.append(estado_actual)
+                # Se compara por todos los hijos
+                for elem in hijos:
+                    if not esta_en_lista(elem, closed_list):
+                        if not esta_en_lista(elem, open_list):
+                            open_list.append(elem)
+                            nodes_gen += 1
+                        else:
+                            op_l_cpy = open_list.copy()
+                            for elem2 in op_l_cpy:
+                                if elem2 == elem:
+                                    if elem2.coste_f > elem.coste_f:
+                                        open_list.remove(elem2)
+                                        open_list.append(elem)
+                                        nodes_gen += 1
+                                else:
+                                    continue
                     else:
-                        for elem2 in open_list:
-                            if elem2 == elem:
-                                if elem2.coste_f > elem.coste_f:
-                                    open_list.remove(elem2)
-                                    open_list.append(elem)
-                else:
-                    continue
+                        continue
 
     if goal:
         print("Solución encontrada")
-        print("Nodos generados: ", nodos_gen)
-        print("Nodos expandidos: ", nodos_expand)
-        print("Coste: ", nodo_actual.coste_f)
-        print("Ruta: ", nodo_actual.id)
-        while nodo_actual.padre is not None:
-            nodo_actual = nodo_actual.padre
-            print("Ruta: ", nodo_actual.id)
+        print("Nodos generados: ", nodes_gen)
+        print("Nodos expandidos: ", len(closed_list))
+        print("Coste Total: ", estado_actual.coste_f)
+        print(f"Ruta: {estado_actual.cola_bus[-1][0]}")
+        while estado_actual.padre is not None:
+            print(estado_actual.cola_bus[-1][0])
+            estado_actual = estado_actual.padre
+    else:
+        print("Solución no encontrada")
 
 
 # La función a_estrella se llama desde el main
 def main():
-    """Función principal del programa"""
+    """ Función principal del programa """
 
-    # Inicialización
-    estado_inicial = open_list[0]
-    nodo_inicial = Nodo(0, estado_inicial, None, heuristica(estado_inicial))
-    open_list.append(nodo_inicial)
+    path = 'ASTAR-tests\\alumnos00.prob'
+    estado_inicial = Estado(None, [], cola_total)
+    a_estrella(estado_inicial, cola_total)
+
+
+if __name__ == '__main__':
+    main()  # Llamamos a la función principal
+
+
+# ! Llamamos a la función para sacar el diccionario de la cola de alumnos del archivo
